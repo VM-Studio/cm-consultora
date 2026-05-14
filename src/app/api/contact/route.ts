7a5eb708a40f61e5db1993c3fc2b7cd6cfb76c99
Juan Cruz Benitez <empresadejj@gmail.com>
2026-05-14 15:35:38 -0300

    fix: from contacto@cmhrstudio.com dominio verificado

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request) {
 
   try {
     await resend.emails.send({
-      from: 'Contacto CM HR Studio <onboarding@resend.dev>',
+      from: 'Contacto CM HR Studio <contacto@cmhrstudio.com>',
       to: 'contacto@cmhrstudio.com',
       subject: `Consulta de ${name}`,
       html: `
